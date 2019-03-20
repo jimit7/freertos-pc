a9e69027fd38524f4fb11699c2f3bf54d73aed2d
jimit7 <jimitypatel1995@gmail.com>
2019-03-20 11:29:09 -0400

    jgj

--- blink/src/blink.c
+++ blink/src/blink.c
@@ -1,6 +1,6 @@
 /*
 ===============================================================================
- Name        : k.c
+ Name        : blink.c
  Author      : $(author)
  Version     :
  Copyright   : $(copyright)
@@ -19,15 +19,7 @@
 #include <cr_section_macros.h>
 
 // TODO: insert other include files here
-Delay(int x) //delay function
-{
-	int j,k;
-	for(j=0;j<1000;j++)
-	{
-	for(k=0;k<x;k++)
-	{}
-		}
-}
+
 // TODO: insert other definitions and declarations here
 
 int main(void) {
@@ -40,30 +32,12 @@ int main(void) {
     // functions related to the board hardware
     Board_Init();
     // Set the LED to the state of "On"
-
-#endif
-#endif
-    while(1){
-
-    Board_LED_Set(0, false);// Set the RED LED to the state of "On" & ALL OTHER OFF
-    Board_LED_Set(1, true);
-    Board_LED_Set(2, true);
-
-    Delay(2000); // delay for 1 sec.
-
-    Board_LED_Set(0, true);//Set the GREEN LED to the state of "On" & ALL OTHER OFF
-    Board_LED_Set(1, false);
-    Board_LED_Set(2, true);
-
-
-    Delay(2000);// delay for 1 sec.
     Board_LED_Set(0, true);
-    Board_LED_Set(1, true);//Set the Blue LED to the state of "On" & ALL OTHER OFF
-    Board_LED_Set(2, false);
-    Delay(2000); //delay for 1 sec.
+#endif
+#endif
 
+    // TODO: insert code here
 
-    }
     // Force the counter to be placed into memory
     volatile static int i = 0 ;
     // Enter an infinite loop, just incrementing a counter
---
+++ freertos_blinky2/Debug/example/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../example/src/cr_startup_lpc175x_6x.c \
+../example/src/freertos_blinky2.c \
+../example/src/sysinit2.c 
+
+OBJS += \
+./example/src/cr_startup_lpc175x_6x.o \
+./example/src/freertos_blinky2.o \
+./example/src/sysinit2.o 
+
+C_DEPS += \
+./example/src/cr_startup_lpc175x_6x.d \
+./example/src/freertos_blinky2.d \
+./example/src/sysinit2.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+example/src/%.o: ../example/src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M3 -I"/home/jpatel/freertos-pc/lpc_chip_175x_6x/inc" -I"/home/jpatel/freertos-pc/lpc_board_nxp_lpcxpresso_1769/inc" -I"/home/jpatel/freertos-pc/freertos_blinky2/example/inc" -I"/home/jpatel/freertos-pc/freertos_blinky2/freertos/inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
